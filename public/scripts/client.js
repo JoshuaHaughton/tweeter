3df6e8bd57a4570ca13470e6ca5669bb07d6e995
Joshua Haughton <itsjoshuahaughton@gmail.com>
2021-11-18 01:29:32 +0000

    Created JS functionality in client.js that creates and renders new tweets using HTML and string templates

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,90 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+//On-ready shorthand
+
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1637024204390
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1637110604390
+  }
+]
+
+
+$(() => {
+  const createTweetElement = function(tweetData) {
+
+    const $tweet = $(`
+    <article>
+
+          <header>
+
+            <div class="name">
+              <img src="${tweetData.user.avatars}"></i>
+              <p>${tweetData.user.name}</p>
+            </div>
+
+            <div>
+              <p>${tweetData.user.handle}</p>
+            </div>
+
+          </header>
+
+          <div class="bottom-border">
+            
+            <p class="tweet-content">${tweetData.content.text}</p>
+            
+            <!-- <textarea type='hidden' cols="30" rows="10" readonly>text here</textarea> -->
+            
+          </div>
+
+          <footer>
+
+            <div>
+              <p class='p1'>${timeago.format(tweetData['created_at'])}</p>
+            </div>
+            
+            <div class='icons'>
+              <i class="fa-solid fa-flag"></i>
+              <i class="fa-solid fa-retweet"></i>
+              <i class="fa-solid fa-heart"></i>
+            </div>
+            
+            <!-- <img src="" alt=""> -->
+          </footer>
+
+
+        </article>`);
+        return $tweet;
+  }
+
+
+  const renderTweets = function(tweets) {
+    for (tweet of tweets) {
+      let $tweet = createTweetElement(tweet)
+      $('.tweet-container').append($tweet)
+    }
+   }
+
+  renderTweets(data)
+
+
+})
